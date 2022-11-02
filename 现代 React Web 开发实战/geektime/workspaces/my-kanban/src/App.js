ea72228e8a40e3488a0899756973159589346aee
guozheng <guozheng07@meituan.com>
2022-11-02 16:14:30 +0800

    feat: 将任务的创建时间由字符串变成相对当前时间的时间

--- 现代 React Web 开发实战/geektime/workspaces/my-kanban/src/App.js
+++ 现代 React Web 开发实战/geektime/workspaces/my-kanban/src/App.js
@@ -3,14 +3,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // 从 @emotion/react 包导入 css 函数
-// 用 CSS-in-JS 代替从 App.css 中引入的类名样式
+// 利用 CSS-in-JS 技术将 React 组件的 CSS 样式也组件化（代替从 App.css 中引入的类名样式）
 // 当你用 @emotion/react 的 css 属性写组件样式时，从框架设计上你可以把 React 内外的变量都插进样式代码里，包括 React 组件的 props、state 和 context。
 import { css } from '@emotion/react';
 
+const MINUTE = 60 * 1000;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const UPDATE_INTERVAL = MINUTE;
 // 循环渲染事项的组件
 const KanbanCard = ({ title, status }) => {
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+    // updateDisplayTime 函数作用：根据卡片创建时间计算相对时间，并每分钟一次设置到 displayTime 上，卡片随即更新。
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status);
+      let relativeTime = '刚刚';
+      if (MINUTE <= timePassed && timePassed < HOUR) {
+        relativeTime = `${Math.ceil(timePassed / MINUTE)} 分钟前`;
+      } else if (HOUR <= timePassed && timePassed < DAY) {
+        relativeTime = `${Math.ceil(timePassed / HOUR)} 小时前`;
+      } else if (DAY <= timePassed) {
+        relativeTime = `${Math.ceil(timePassed / DAY)} 天前`;
+      }
+      setDisplayTime(relativeTime);
+    };
+    // setTimeout 函数用来指定某个函数或某段代码，在多少毫秒之后执行。
+    // setInterval 指定 updateDisplayTime 函数每隔 60s 就执行一次，也就是无限次的定时执行。
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    // updateDisplayTime 作为 useEffect 第一个参数的回调函数，在组件首次挂载时会被调用。
+    updateDisplayTime();
+
+    // updateDisplayTime 回调函数的返回值是另一个 cleanup 函数，负责在组件被卸载时清除定时器。
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+  }, [status]);
+
   return (
     // <li className="kanban-card">
     //   <div className="card-title">{title}</div>
@@ -22,10 +53,11 @@ const KanbanCard = ({ title, status }) => {
         text-align: right;
         font-size: 0.8rem;
         color: #333;
-      `}>{status}</div>
+      `} title={status}>{displayTime}</div>
     </li>
   );
 };
+
 // 待处理看板 -> 添加新的待办事项
 const KanbanNewCard = ({ onSubmit }) => {
   const [title, setTitle] = useState('');
@@ -103,7 +135,7 @@ const kanbanCardTitleStyle = css`
 const COLUMN_BG_COLORS = {
   todo: '#C9AF97',
   ongoing: '#FFE799;',
-  done: 'C0E8BA',
+  done: '#C0E8BA',
 }
 
 // 将 section 标签也写成 React 组件
@@ -161,19 +193,19 @@ function App() {
   // showAdd 的初始值为 false，改变其值的方法是 setShowAdd
   const [showAdd, setShowAdd] = useState(false);
   const [todoList, setTodoList] = useState([
-    { title: '开发任务-1', status: '22-05-22 18:15' },
-    { title: '开发任务-3', status: '22-05-22 18:15' },
-    { title: '开发任务-5', status: '22-05-22 18:15' },
-    { title: '测试任务-3', status: '22-05-22 18:15' }
+    { title: '开发任务-1', status: '2022-11-02 18:15' },
+    { title: '开发任务-3', status: '2022-11-01 18:15' },
+    { title: '开发任务-5', status: '2022-05-22 18:15' },
+    { title: '测试任务-3', status: '2022-05-22 18:15' }
   ]);
   const [ongoingList] = useState([
-    { title: '开发任务-4', status: '22-05-22 18:15' },
-    { title: '开发任务-6', status: '22-05-22 18:15' },
-    { title: '测试任务-2', status: '22-05-22 18:15' }
+    { title: '开发任务-4', status: '2022-05-22 18:15' },
+    { title: '开发任务-6', status: '2022-05-22 18:15' },
+    { title: '测试任务-2', status: '2022-05-22 18:15' }
   ]);
   const [doneList] = useState([
-    { title: '开发任务-2', status: '22-05-22 18:15' },
-    { title: '测试任务-1', status: '22-05-22 18:15' }
+    { title: '开发任务-2', status: '2022-05-22 18:15' },
+    { title: '测试任务-1', status: '2022-05-22 18:15' }
   ]);
 
   const handleAdd = (evt) => {
